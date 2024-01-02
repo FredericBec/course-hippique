7c5e93fd9d0e28aaba815c652d47930470ca7d57
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-02 15:59:55 +0100

    ajout scanner, getHorses et tierce

--- src/HorseRun.java
+++ src/HorseRun.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class HorseRun {
+	
+	public static final int minHorses = 12;
+	public static final int maxHorses = 20;
 
 	public static void main(String[] args) {
 		
+		Scanner scan = new Scanner(System.in);
+		
+		
 
 	}
+	
 
 }
+
+
+/*
+ *Besoin : permettre d'afficher une arrivée tirée aléatoirement en fonction du nombre de chevaux et du type de la course.
+ *
+ *Type de course: tiercé, quarté et quinté (respectivement 3, 4 et 5 gagnants)
+ *nombre de chevaux et type déterminé par l'utilisateur
+ *
+ */
