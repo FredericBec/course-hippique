c9c09963b1f753bb3fb45c8e53813151d9d1cb30
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-03 07:58:19 +0100

    maj commit ajout scanner, getHorses et tierce

--- src/HorseRun.java
+++ src/HorseRun.java
@@ -4,13 +4,29 @@ public class HorseRun {
 	
 	public static final int minHorses = 12;
 	public static final int maxHorses = 20;
+	
+	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
+		
 		
 		
 
+	}
+	
+	public static int getHorses() {
+		System.out.println("Veuillez choisir le nombre de chevaux");
+		int horseNumber = scan.nextInt();
+		
+		return horseNumber;
+	}
+	
+	public static void tierce(){
+		
+		for(int i = 0; i < getHorses(); i++) {
+			
+		}
 	}
 	
 
