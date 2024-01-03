37846a5b1d14e483d0011cad7c2566a8d88a37ac
Frederic Bec <bec.frederic90@outlook.fr>
2024-01-03 09:18:51 +0100

    ajout méthode validInput

--- src/HorseRun.java
+++ src/HorseRun.java
@@ -2,62 +2,73 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class HorseRun {
-	
+
 	public static final int minHorses = 12;
 	public static final int maxHorses = 20;
-	
+
 	public static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		
-		
-		//tierce();
-		//quarte();
-		quinte();
-	}
-	
-	public static int getHorses() {
+
 		System.out.println("Veuillez choisir le nombre de chevaux");
-		int horseNumber = scan.nextInt();
-		
-		return horseNumber;
+		int horseNumber = validInput(scan, "Veuillez entrer entrer un nombre !!!");
+		tierce(horseNumber);
+		// quarte();
+		// quinte();
 	}
-	
-	public static void tierce(){
-		
-		int[] winnerTierce = new Random().ints(1, getHorses()).distinct().limit(3).toArray();
-		
-		for(int i = 0; i < winnerTierce.length; i++) {
+
+	public static int validInput(Scanner scan, String errorMessage) {
+		int input = 0;
+		boolean validInput = false;
+
+		while (!validInput) {
+			if (scan.hasNextInt()) {
+				input = scan.nextInt();
+				validInput = true;
+			} else {
+				System.out.println(errorMessage);
+				System.out.println("Veuillez choisir le nombre de chevaux");
+				scan.next();
+			}
+		}
+
+		return input;
+	}
+
+	public static void tierce(int horse) {
+
+		int[] winnerTierce = new Random().ints(1, horse).distinct().limit(3).toArray();
+
+		for (int i = 0; i < winnerTierce.length; i++) {
 			System.out.print(winnerTierce[i] + " - ");
 		}
 	}
-	
-	public static void quarte() {
-		
-		int[] winnerQuarte = new Random().ints(1, getHorses()).distinct().limit(4).toArray();
-		
-		for(int i = 0; i < winnerQuarte.length; i++) {
+
+	public static void quarte(int horse) {
+
+		int[] winnerQuarte = new Random().ints(1, horse).distinct().limit(4).toArray();
+
+		for (int i = 0; i < winnerQuarte.length; i++) {
 			System.out.print(winnerQuarte[i] + " - ");
 		}
 	}
-	
-	public static void quinte() {
-		
-		int[] winnerQuinte = new Random().ints(1, getHorses()).distinct().limit(5).toArray();
-		
-		for(int i = 0; i < winnerQuinte.length; i++) {
+
+	public static void quinte(int horse) {
+
+		int[] winnerQuinte = new Random().ints(1, horse).distinct().limit(5).toArray();
+
+		for (int i = 0; i < winnerQuinte.length; i++) {
 			System.out.print(winnerQuinte[i] + " - ");
 		}
 	}
-	
 
 }
 
-
 /*
- *Besoin : permettre d'afficher une arrivée tirée aléatoirement en fonction du nombre de chevaux et du type de la course.
+ * Besoin : permettre d'afficher une arrivée tirée aléatoirement en fonction du
+ * nombre de chevaux et du type de la course.
  *
- *Type de course: tiercé, quarté et quinté (respectivement 3, 4 et 5 gagnants)
- *nombre de chevaux et type déterminé par l'utilisateur
+ * Type de course: tiercé, quarté et quinté (respectivement 3, 4 et 5 gagnants)
+ * nombre de chevaux et type déterminé par l'utilisateur
  *
  */
